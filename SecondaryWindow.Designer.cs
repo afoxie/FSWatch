3ebbc0cde13a6afedae64b43f87c15893427abd1
Afo <57149309+afoxie@users.noreply.github.com>
2021-05-26 23:33:25 -0400

    Revert "Revert "Initial commit""
    This reverts commit ead4a6b50384bfb9314e4fc8c36f5cb52efef1ee.

---
+++ SecondaryWindow.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace FSWatch
+{
+    partial class SecondaryWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SecondaryWindow));
+            this.fileList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // fileList
+            // 
+            this.fileList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileList.BackColor = System.Drawing.SystemColors.Control;
+            this.fileList.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileList.FormattingEnabled = true;
+            this.fileList.ItemHeight = 21;
+            this.fileList.Location = new System.Drawing.Point(0, 0);
+            this.fileList.Margin = new System.Windows.Forms.Padding(4);
+            this.fileList.Name = "fileList";
+            this.fileList.Size = new System.Drawing.Size(584, 361);
+            this.fileList.TabIndex = 0;
+            // 
+            // SecondaryWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ControlBox = false;
+            this.Controls.Add(this.fileList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "SecondaryWindow";
+            this.Text = "FSW_LOG";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ListBox fileList;
+    }
+}
